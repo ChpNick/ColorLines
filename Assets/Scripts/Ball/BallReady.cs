4c7d650324a522b10ebfe7ab894a92cd13c9bef7
Nikolay Pechenin <nikolay.pechenin@migogroup.ru>
2020-04-16 21:07:39 +0300

    add bouncy ball

---
+++ Assets/Scripts/Ball/BallReady.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReady : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _ready = false;
+
+    private static readonly int Ready = Animator.StringToHash("Ready");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        _ready = !_ready;
+        _animator.SetBool(Ready, _ready);
+    }
+}
